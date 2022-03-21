74bac6b0c4d1f2ef42a595350e689a9069b36404
unknown <laralourenco5966@gmail.com>
2022-03-21 18:44:59 -0300

    Estilização simples com CSS

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://i.imgur.com/ZuPBHI4.png" alt="Icone genérico" className='img'/>
+        </>
+    );
+}
+
+export default Home;
