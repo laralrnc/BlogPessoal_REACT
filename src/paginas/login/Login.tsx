554c25401e8d64d33eb998dd8e498a832d6f765d
unknown <laralourenco5966@gmail.com>
2022-03-28 20:42:28 -0300

    Comunicação com o Back-end-Login-Componente

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,59 +1,75 @@
-import React, {useState, ChangeEvent} from 'react';
+import React, { useState, ChangeEvent, useEffect } from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
-import {Link} from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { api } from '../../services/Service';
 import './Login.css';
 import UserLogin from '../../models/UserLogin';
 
 
 function Login() {
 
-    const[userLogin, setUserLogin] = useState<UserLogin>({
+    let history = useHistory();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>({
         id: 0,
         usuario: '',
         senha: '',
-        token:''
+        token: ''
     })
 
     //utilizada para atualziar a model quando o usuário digitar os dados
     //[e.target.name] : e.target.value captura o valor digitado pelo usuário
-    function updateModel(e: ChangeEvent <HTMLInputElement>) {
+    function updateModel(e: ChangeEvent<HTMLInputElement>) {
 
         setUserLogin({
             ...userLogin,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
-    async function onSubmit(e: ChangeEvent <HTMLFormElement>){
+    useEffect(
+        () => {
+            if (token != '') {
+                history.push('/home')
+            }
+        }, [token]
+    )
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
         //impede que o botão atualize a tela
         e.preventDefault();
+        try {
+            const resposta = await api.post(`/usuarios/logar`, userLogin)
+            setToken(resposta.data.token)
+            alert('usuário logado!');
+        } catch (error) {
+            alert('dados incorretor. erro ao logar!');
+        }
 
-        console.log('userLogin: ' + userLogin);
     }
 
     return (
-        <Grid container direction = 'row' justifyContent='center' alignItems='center'>
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid xs={6} alignItems='center'>
                 <Box paddingX={20}>
                     <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>
                             Entrar</Typography>
-                            <TextField value={userLogin.usuario} onChange={(e: ChangeEvent <HTMLInputElement>) =>updateModel(e)} id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                            <TextField value={userLogin.senha} onChange={(e: ChangeEvent <HTMLInputElement>) =>updateModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
-                            <Box marginTop={2} textAlign='center'>
-                                <Link to ='/home' className='text-decorator-none'>
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Logar
-                                </Button>
-                                </Link>
-                            </Box>
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                            <Button type='submit' variant='contained' color='primary'>
+                                Logar
+                            </Button>
+                        </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
-                            <Typography variant = 'subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                        <Link to ='/cadastrousuario'>
-                            <Typography variant = 'subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        <Link to='/cadastrousuario'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
                         </Link>
                     </Box>
 
