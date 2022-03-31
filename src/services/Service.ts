01365ccbe5c33450e9b0582df0cbb7d5b91304fa
unknown <laralourenco5966@gmail.com>
2022-03-31 14:28:30 -0300

    Service buscaTema e buscaPostagem + Model tema e postagem

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -15,3 +15,10 @@ export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data)
 }
+
+//serve para lista tema e lista postagem
+export const busca = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDado(resposta.data)
+}
+
